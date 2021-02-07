076cc2490fea564493d935de86c2abbc25780ae2
pc2005 <philchen2005@gmail.com>
2021-02-06 20:41:28 -0500

    Test message passing.

--- pc_msg_net.py
+++ pc_msg_net.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn.functional as F
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops, degree
+from torch_geometric.datasets import Planetoid
 
 class GCNConv(MessagePassing):
     def __init__(self, in_channels, out_channels):
@@ -32,3 +34,27 @@ class GCNConv(MessagePassing):
         # Step 4: Normalize node features.
         return norm.view(-1, 1) * x_j
 
+# load data
+dataset = Planetoid(root='/tmp/Cora', name='Cora')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = GCNConv(dataset.num_node_features, dataset.num_classes).to(device)
+data = dataset[0].to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+# model train
+model.train()
+for epoch in range(200):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+# model evaluation
+model.eval()
+_, pred = model(data).max(dim=1)
+correct = int(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+acc = correct / int(data.test_mask.sum())
+print('Accuracy: {:.4f}'.format(acc))
+
+print('Job done')
