5ac64c95123514c6c97034ecce45283fe0597795
pc2005 <philchen2005@gmail.com>
2021-05-19 21:32:39 -0400

    Add cs224w_0 colab.

---
+++ pc_cs224w_0.py
@@ -0,0 +1,199 @@
+# %%
+import networkx as nx
+import torch
+import matplotlib.pyplot as plt
+
+# %% Graph
+# Create an undirected graph G
+G = nx.Graph()
+print(G.is_directed())
+
+# Create a directed graph H
+H = nx.DiGraph()
+print(H.is_directed())
+
+# Add graph level attribute
+G.graph["Name"] = "Bar"
+print(G.graph)
+
+# %% Node
+# Add one node with node level attributes
+G.add_node(0, feature=0, label=0)
+
+# Get attributes of the node 0
+node_0_attr = G.nodes[0]
+print("Node 0 has the attributes {}".format(node_0_attr))
+
+# Add multiple nodes with attributes
+G.add_nodes_from([
+  (1, {"feature": 1, "label": 1}),
+  (2, {"feature": 2, "label": 2})
+])
+
+# Loop through all the nodes
+# Set data=True will return node attributes
+for node in G.nodes(data=True):
+  print(node)
+
+# Get number of nodes
+num_nodes = G.number_of_nodes()
+print("G has {} nodes".format(num_nodes))
+
+# %% Edge
+# Add one edge with edge weight 0.5
+G.add_edge(0, 1, weight=0.5)
+
+# Get attributes of the edge (0, 1)
+edge_0_1_attr = G.edges[(0, 1)]
+print("Edge (0, 1) has the attributes {}".format(edge_0_1_attr))
+
+# Add multiple edges with edge weights
+G.add_edges_from([
+  (1, 2, {"weight": 0.3}),
+  (2, 0, {"weight": 0.1})
+])
+
+# Loop through all the edges
+# Here there is no data=True, so only the edge will be returned
+for edge in G.edges():
+  print(edge)
+
+# Get number of edges
+num_edges = G.number_of_edges()
+print("G has {} edges".format(num_edges))
+
+# %% Visualization
+# Draw the graph
+nx.draw(G, with_labels = True)
+
+# %% Node Degree and Neighbor
+
+node_id = 1
+
+# Degree of node 1
+print("Node {} has degree {}".format(node_id, G.degree[node_id]))
+
+# Get neighbor of node 1
+for neighbor in G.neighbors(node_id):
+  print("Node {} has neighbor {}".format(node_id, neighbor))
+
+# %% Misc
+num_nodes = 4
+# Create a new path like graph and change it to a directed graph
+G = nx.DiGraph(nx.path_graph(num_nodes))
+nx.draw(G, with_labels = True)
+
+# Get the PageRank
+pr = nx.pagerank(G, alpha=0.8)
+pr
+
+# %%
+# Helper function for visualization.
+
+# Visualization function for NX graph or PyTorch tensor
+def visualize(h, color, epoch=None, loss=None):
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+
+    if torch.is_tensor(h):
+        h = h.detach().cpu().numpy()
+        plt.scatter(h[:, 0], h[:, 1], s=140, c=color, cmap="Set2")
+        if epoch is not None and loss is not None:
+            plt.xlabel(f'Epoch: {epoch}, Loss: {loss.item():.4f}', fontsize=16)
+    else:
+        nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
+                         node_color=color, cmap="Set2")
+
+    plt.show()
+
+# %% PyG Dataset
+from torch_geometric.datasets import KarateClub
+
+dataset = KarateClub()
+print(f'Dataset: {dataset}:')
+print('======================')
+print(f'Number of graphs: {len(dataset)}')
+print(f'Number of features: {dataset.num_features}')
+print(f'Number of classes: {dataset.num_classes}')
+
+data = dataset[0]  # Get the first graph object.
+
+print(data)
+print('==============================================================')
+
+# Gather some statistics about the graph.
+print(f'Number of nodes: {data.num_nodes}')
+print(f'Number of edges: {data.num_edges}')
+print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+print(f'Number of training nodes: {data.train_mask.sum()}')
+print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
+print(f'Contains isolated nodes: {data.contains_isolated_nodes()}')
+print(f'Contains self-loops: {data.contains_self_loops()}')
+print(f'Is undirected: {data.is_undirected()}')
+
+# %% PyG Data
+edge_index = data.edge_index
+print(edge_index.t())
+
+# %% PyG Edge Index
+from torch_geometric.utils import to_networkx
+
+G = to_networkx(data, to_undirected=True)
+visualize(G, color=data.y)
+# %%
+import torch
+from torch.nn import Linear
+from torch_geometric.nn import GCNConv
+
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super(GCN, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = GCNConv(dataset.num_features, 4)
+        self.conv2 = GCNConv(4, 4)
+        self.conv3 = GCNConv(4, 2)
+        self.classifier = Linear(2, dataset.num_classes)
+
+    def forward(self, x, edge_index):
+        h = self.conv1(x, edge_index)
+        h = h.tanh()
+        h = self.conv2(h, edge_index)
+        h = h.tanh()
+        h = self.conv3(h, edge_index)
+        h = h.tanh()  # Final GNN embedding space.
+        
+        # Apply a final (linear) classifier.
+        out = self.classifier(h)
+
+        return out, h
+
+model = GCN()
+print(model)
+
+_, h = model(data.x, data.edge_index)
+print(f'Embedding shape: {list(h.shape)}')
+
+visualize(h, color=data.y)
+
+# %% PyG GCN Training
+import time
+
+criterion = torch.nn.CrossEntropyLoss()  # Define loss criterion.
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # Define optimizer.
+
+def train(data):
+    optimizer.zero_grad()  # Clear gradients.
+    out, h = model(data.x, data.edge_index)  # Perform a single forward pass.
+    loss = criterion(out[data.train_mask], data.y[data.train_mask])  # Compute the loss solely based on the training nodes.
+    loss.backward()  # Derive gradients.
+    optimizer.step()  # Update parameters based on gradients.
+    return loss, h
+
+for epoch in range(401):
+    loss, h = train(data)
+    # Visualize the node embeddings every 10 epochs
+    if epoch % 10 == 0:
+        visualize(h, color=data.y, epoch=epoch, loss=loss)
+        time.sleep(0.3)
+# %%
